dfe61266076c32e38297e1e79975e38821a81476
pruth_17 <ppruthvi2017@gmail.com>
2020-06-06 17:32:19 -0700

    added files

---
+++ Quizzler-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Pruthvi Punwar on 6/4/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+}
