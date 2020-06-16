429fa4cb1e35c0b4bbdb18093102498da9b12775
pruth_17 <ppruthvi2017@gmail.com>
2020-06-15 17:14:57 -0700

    updated

--- BMI-Calculator-iOS13/BMI Calculator/SceneDelegate.swift
+++ BMI-Calculator-iOS13/BMI Calculator/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  BMI Calculator
 //
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
+//  Created by Pruthvi Punwar on 21/08/2019.
+//  Copyright © 2020 Pruthvi Punwar. All rights reserved.
 //
 
 import UIKit
